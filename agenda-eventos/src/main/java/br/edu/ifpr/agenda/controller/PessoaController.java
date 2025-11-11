41e5a3d5ac1f7d2744b68ec9fdcad752a90b2d2e
GabrielMartins <senqueiogabriel@gmail.com>
2025-11-11 08:59:27 -0300

    falta finalizar

--- agenda-eventos/src/main/java/br/edu/ifpr/agenda/controller/PessoaController.java
+++ agenda-eventos/src/main/java/br/edu/ifpr/agenda/controller/PessoaController.java
@@ -12,14 +12,22 @@ public class PessoaController {
 
     public void cadastrarPessoa(Pessoa pessoa){
         if(pessoa.getNome() == null  || pessoa.getNome().isEmpty()){
-            System.out.println("Nome não pode ser vazio");
+            System.out.println("Nome não pode ser nulo");
+            return;
+        }
+        if(pessoa.getCpf() == null  || pessoa.getCpf().isEmpty()){
+            System.out.println("CPF não pode ser nulo");
+            return;
+        }
+        if (pessoa.getTelefone() == null || pessoa.getTelefone().isEmpty()) {
+            System.out.println("Telefone não pode de nulo");
+        }
+        if(pessoa.getEmail() == null  || pessoa.getEmail().isEmpty()){
+            System.out.println("E-mail não pode ser nulo");
             return;
         }
 
         dao.salvarPessoa(pessoa);
-        
+
     }
-
-    
-
 }
